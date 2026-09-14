daf54f071225047df7318929ba5bbc446f0f9135
Timothy Partee <47483875+TimothyP791@users.noreply.github.com>
2026-09-14 12:21:15 -0700

    Initial ASP.NET Core MVC project

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CountyServicePortal.Models;
+
+namespace CountyServicePortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ServiceRequest> ServiceRequests { get; set; }
+    }
+}
--- CountyServicePortal/Models/ServiceRequest.cs
+++ CountyServicePortal/Models/ServiceRequest.cs
@@ -1,13 +1,26 @@
+
+using System.ComponentModel.DataAnnotations;
+
 namespace CountyServicePortal.Models
 {
     public class ServiceRequest
     {
+        [Key] // Primary key for the ServiceRequest entity
         public int RequestId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public string Status { get; set; }
-        public DateTime DateSubmitted { get; set; }
 
+        [Required] // Attributes used to help EF Core creat proper database Schema and to validate the model data
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime DateSubmitted { get; set; }
     }
 }
