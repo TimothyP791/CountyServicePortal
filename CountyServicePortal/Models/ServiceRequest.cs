176e989f99f4448c513e8677d948bb02d24e12e6
Timothy Partee <47483875+TimothyP791@users.noreply.github.com>
2026-09-14 13:46:47 -0700

    Data and Migration Folders

--- CountyServicePortal/Models/ServiceRequest.cs
+++ CountyServicePortal/Models/ServiceRequest.cs
@@ -10,16 +10,16 @@ namespace CountyServicePortal.Models
 
         [Required] // Attributes used to help EF Core creat proper database Schema and to validate the model data
         [StringLength(100)]
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
         [Required]
         [StringLength(1000)]
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
         [Required]
-        public string Category { get; set; }
+        public string Category { get; set; } = string.Empty;
 
-        public string Status { get; set; }
+        public string Status { get; set; } = "Submitted"; // Default status for new service requests 
 
         public DateTime DateSubmitted { get; set; }
     }
